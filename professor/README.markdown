e70b0b37e9a1d4578a8477def779eca2ecfda974
kiran shrinivaas <hayabuzakiran@gmail.com>
2022-06-25 18:38:37 +0530

    name-modified

--- professor/README.markdown
+++
@@ -1,5 +0,0 @@
-# Professor
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/kiranshrinivaas8367/pen/QWqopWy](https://codepen.io/kiranshrinivaas8367/pen/QWqopWy).
-
-
--- survey/dist/script.js
+++ survey/dist/script.js
@@ -1,27 +1,41 @@
-// !! IMPORTANT README:
+const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#email");
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+const re = {
+  "name": /^[a-z]*$/i,
+  "email": /^[\w]*@[a-z]*\.[a-z]{2,}$/
+}
 
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
+nameField.addEventListener("input" ,(e) => {
+  const val = e.currentTarget.value;
+  const isMatch = re.name.test(val);
+  console.log(isMatch)
+  console.log(val)
 
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
+  if(isMatch &&val !== ''){
+    console.log(val)
+    e.target.classList.add("valid");
+    e.target.classList.remove("invalid");
+  }
+  else{
+    e.target.classList.remove("valid");
+    e.target.classList.add("invalid");
+  }
+});
 
-// Once you have read the above messages, you can delete all comments.
+emailField.addEventListener("input", (e) => {
+  const val = e.currentTarget.value;
+  const isMatch = re.email.test(val);
+  console.log(isMatch)
+  console.log(val)
+
+  if(isMatch &&val !== ''){
+    console.log(val)
+    e.target.classList.add("valid");
+    e.target.classList.remove("invalid");
+  }
+  else{
+    e.target.classList.remove("valid");
+    e.target.classList.add("invalid");
+  }
+})
